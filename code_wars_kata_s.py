646d31d4f3206084318072d6c23d6f868d1dc882
Kamil Handzlik <kamol0209@gmail.com>
2024-08-22 19:48:49 +0200

    My daily practice.

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -63,19 +63,21 @@ piece of JavaScript to calculate the amount of water your plants will need when 
 amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs.
 Your task is to debug the code before your plants die!"""
 
+
 # original code for debugging
 def rain_amount(mm):
     if rain_amount == 40:
-         return "You need to give your plant " + {rain_amount - 40} + " mm of water"
+        return "You need to give your plant " + {rain_amount - 40} + " mm of water"
     else:
-         return "Your plant has had more than enough water for today!"
+        return "Your plant has had more than enough water for today!"
 
-#solution
+
+# solution
 def rain_amount(mm):
     if mm < 40:
-         return f"You need to give your plant {40 - mm}mm of water"
+        return f"You need to give your plant {40 - mm}mm of water"
     else:
-         return "Your plant has had more than enough water for today!"
+        return "Your plant has had more than enough water for today!"
 
 
 #########################################################################################################
@@ -91,6 +93,7 @@ replace("!Hi! Hi!") === "!H!! H!!"
 replace("aeiou") === "!!!!!"
 replace("ABCDE") === "!BCD!"""
 
+
 def replace_exclamation(st):
     vowels = 'aeiouAEIOU'
     result = ''
@@ -100,7 +103,6 @@ def replace_exclamation(st):
         else:
             result += char
     return result
-
 
 
 #########################################################################################################
@@ -127,6 +129,7 @@ Input and Output Examples
 expressionsMatter(1, 2, 3) ==> 9, because (1 + 2) * 3 = 9.
 expressionsMatter(1, 1, 1) ==> 3, because 1 + 1 + 1 = 3.
 expressionsMatter(9, 1, 1) ==> 18, because 9 * (1 + 1) = 18."""
+
 
 def expressions_matter(a, b, c):
     return max(
@@ -156,6 +159,7 @@ Assertion messages may be unclear about what they display in some languages. If 
 
 """
 
+
 def duplicate_encode(word):
     word = word.lower()
     result = ""
@@ -165,7 +169,6 @@ def duplicate_encode(word):
         else:
             result += '('
     return result
-
 
 
 #########################################################################################################
@@ -215,6 +218,7 @@ Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 
 """
 
+
 def pipe_fix(nums):
     return list(range(nums[0], nums[-1] + 1))
 
@@ -247,16 +251,17 @@ def flatten_and_sort(array):
 """This kata is about multiplying a given number by eight if it is an even number and by nine otherwise."""
 
 
-#Rozwiązanie 1
-def simple_multiplication(number) :
+# Rozwiązanie 1
+def simple_multiplication(number):
     if number % 2 != 0:
         return number * 9
     else:
         return number * 8
 
-#Rozwiązanie 2
 
-def simple_multiplication(number) :
+# Rozwiązanie 2
+
+def simple_multiplication(number):
     return number * 9 if number % 2 != 0 else number * 8
 
 
@@ -294,5 +299,84 @@ def simple_multiplication(number) :
     ⣿⡿⠋⠁⠀⠀⢀⣀⣠⡴⣸⣿⣇⡄⠀⠀⠀⠀⢀⡿⠄⠙⠛⠀⣀⣠⣤⣤⠄
     
     """
+
+
 def square(n):
     return n ** 2
+
+
+#########################################################################################################
+##################################            11            #############################################
+#########################################################################################################
+
+
+"""A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. Each book has a code c of 3, 4, 5 or more characters. The 1st character of a code is a capital letter which defines the book category.
+
+In the bookseller's stocklist each code c is followed by a space and by a positive integer n (int n >= 0) which indicates the quantity of books of this code in stock.
+
+For example an extract of a stocklist could be:
+
+L = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}.
+or
+L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] or ....
+You will be given a stocklist (e.g. : L) and a list of categories in capital letters e.g :
+
+M = {"A", "B", "C", "W"} 
+or
+M = ["A", "B", "C", "W"] or ...
+and your task is to find all the books of L with codes belonging to each category of M and to sum their quantity according to each category.
+
+For the lists L and M of example you have to return the string (in Haskell/Clojure/Racket/Prolog a list of pairs):
+
+(A : 20) - (B : 114) - (C : 50) - (W : 0)
+where A, B, C, W are the categories, 20 is the sum of the unique book of category A, 114 the sum corresponding to "BKWRK" and "BTSQZ", 50 corresponding to "CDXEF" and 0 to category 'W' since there are no code beginning with W.
+
+If L or M are empty return string is "" (Clojure/Racket/Prolog should return an empty array/list instead).
+
+Notes:
+In the result codes and their values are in the same order as in M.
+See "Samples Tests" for the return."""
+
+
+def stock_list(list_of_art, list_of_cat):
+    if not list_of_art or not list_of_cat:
+        return ""
+
+    category_sums = {cat: 0 for cat in list_of_cat}
+
+    for book in list_of_art:
+        code, quantity = book.split()
+        category = code[0]
+        quantity = int(quantity)
+
+        if category in category_sums:
+            category_sums[category] += quantity
+
+    result = " - ".join(f"({cat} : {category_sums[cat]}" for cat in list_of_cat)
+    return result
+
+
+#########################################################################################################
+##################################            12            #############################################
+#########################################################################################################
+
+"""Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+("Hello", 'o')  =>  1
+("Hello", 'l')  =>  2
+("", 'z')       =>  0
+Notes
+The first argument can be an empty string
+In languages with no distinct character data type, the second argument will be a string of length 1"""
+
+
+def str_count(strng, letter):
+    return strng.count(letter)
+
+#########################################################################################################
+##################################            13            #############################################
+#########################################################################################################
+
+
