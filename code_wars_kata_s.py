3cced68efb47615b5956f0942b1494a79854cc8e
Kamil Handzlik <kamol0209@gmail.com>
2024-08-31 21:11:08 +0200

    My daily practice.

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -882,7 +882,7 @@ def get_real_floor(n):
 
     return result
 
-#seconf version
+#second version
 def get_real_floor(n):
     if n <= 0:
         return n
@@ -890,3 +890,36 @@ def get_real_floor(n):
         return n - 1
     else:
         return n - 2
+
+
+#########################################################################################################
+##################################            26            #############################################
+#########################################################################################################
+
+
+"""Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+
+"""
+
+#solution 1
+def correct(s):
+    mistakes = {'5': 'S', '0': 'O', '1': 'I'}
+
+    for mistake, proper in mistakes.items():
+        s = s.replace(mistake, proper)
+
+    return s
+
+
+#solution 2
+def correct(string):
+    return string.translate(str.maketrans("501", "SOI"))
