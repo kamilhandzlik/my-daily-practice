31f2e5e935ba6f6c6f4d0f6fc23d327e050f2519
Kamil Handzlik <kamol0209@gmail.com>
2024-09-09 20:20:12 +0200

    My daily practice.

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -1232,7 +1232,6 @@ sum_pairs([10, 5, 2, 3, 7, 5],         10)
 == [3, 7]
 Negative numbers and duplicate numbers can and will appear."""
 
-
 def sum_pairs(ints, s):
     seen = set()
     for num in ints:
@@ -1241,3 +1240,108 @@ def sum_pairs(ints, s):
             return [target, num]
         seen.add(num)
     return None
+
+
+
+
+#########################################################################################################
+##################################            36            #############################################
+#########################################################################################################
+
+"""An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+
+"""
+
+def is_anagram(test, original):
+    return sorted(test.lower()) == sorted(original.lower())
+
+
+#########################################################################################################
+##################################            37            #############################################
+#########################################################################################################
+"""Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = Ball()
+ball2 = Ball("super")
+ball1.ball_type  #=> "regular"
+ball2.ball_type  #=> "super"""
+
+
+class Ball:
+    def __init__(self, ball_type='regular'):
+        self.ball_type = ball_type
+
+
+#########################################################################################################
+##################################            38            #############################################
+#########################################################################################################
+
+"""Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+
+Examples:
+
+encrypt("012345", 1)  =>  "135024"
+encrypt("012345", 2)  =>  "135024"  ->  "304152"
+encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+encrypt("01234", 1)  =>  "13024"
+encrypt("01234", 2)  =>  "13024"  ->  "32104"
+encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+Together with the encryption function, you should also implement a decryption function which reverses the process.
+
+If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+
+"""
+def encrypt(text, n):
+    if not text or n <= 0:
+        return text
+
+    for _ in range(n):
+        odd_chars = text[1::2]
+        even_chars = text[0::2]
+        text = odd_chars + even_chars
+    return text
+
+
+def decrypt(encrypted_text, n):
+    if not encrypted_text or n <= 0:
+        return encrypted_text
+
+    lenght = len(encrypted_text)
+
+    for _ in range(n):
+        mid = lenght // 2
+        odd_chars = encrypted_text[:mid]
+        even_chars = encrypted_text[mid:]
+
+        decrypted_text = []
+        odd_index = 0
+        even_index = 0
+
+        for i in range(lenght):
+            if i % 2 == 0:
+                decrypted_text.append(even_chars[even_index])
+                even_index += 1
+            else:
+                decrypted_text.append(odd_chars[odd_index])
+                odd_index += 1
+
+        encrypted_text = ''.join(decrypted_text)
+
+    return encrypted_text
+
+    return
+
+
+
