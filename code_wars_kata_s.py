8a5e26c9d14dcaed5c32514ba248b723d185feb4
Kamil Handzlik <kamol0209@gmail.com>
2024-09-25 19:25:07 +0200

    My daily practice

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -1935,7 +1935,8 @@ total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
 The museum of incredibly dull things wants to get rid of some exhibits. Miriam, the interior architect, comes up with a plan to remove the most boring exhibits. She gives them a rating, and then removes the one with the lowest rating.
 
 However, just as she finished rating all exhibits, she's off to an important fair, so she asks you to write a program that tells her the ratings of the exhibits after removing the lowest one. Fair enough.
-
+ 
+ 
 Task
 Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list.
 
