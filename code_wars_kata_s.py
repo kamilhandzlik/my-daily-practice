0be5a386ee157c1b8f64ac39c1be435e39db5755
Kamil Handzlik <kamol0209@gmail.com>
2024-10-09 19:19:53 +0200

    my daily practice

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -2082,11 +2082,11 @@ def diamond(n):
     # Upper half of diamond
     for i in range(mid + 1):
         spaces = mid - i
-        stars = 2 * i +1
+        stars = 2 * i + 1
         result += ' ' * spaces + '*' * stars + '\n'
 
     # Down half of diamond
-    for i in range(mid - 1, -1 , -1):
+    for i in range(mid - 1, -1, -1):
         spaces = mid - i
         stars = 2 * i + 1
         result += ' ' * spaces + '*' * stars + '\n'
@@ -2097,3 +2097,29 @@ def diamond(n):
 #########################################################################################################
 ##################################            55            #############################################
 #########################################################################################################
+
+"""A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
+
+"""
+
+
+def data_reverse(data):
+    segments = [data[i:i + 8] for i in range(0, len(data), 8)]
+    reversed_segments = segments[::-1]
+    result = [bit for segment in reversed_segments for bit in segment]
+    return result
