77ec277710d4b347a92ec1fdb57cbf4dde60ec74
Kamil Handzlik <kamol0209@gmail.com>
2024-10-13 19:07:31 +0200

    my daily practice

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -2161,7 +2161,7 @@ def people_with_age_drink(age):
 
 
 #########################################################################################################
-##################################            56            #############################################
+##################################            57            #############################################
 #########################################################################################################
 
 """Description:
@@ -2188,7 +2188,7 @@ def remove(s):
 
 
 #########################################################################################################
-##################################            56            #############################################
+##################################            58            #############################################
 #########################################################################################################
 
 """When provided with a letter, return its position in the alphabet.
@@ -2218,4 +2218,15 @@ def position_polish(alphabet):
 def position(alphabet):
     return f"Position of alphabet:{ord(alphabet) - ord('a') + 1}"
 
+#########################################################################################################
+##################################            59            #############################################
+#########################################################################################################
 
+"""You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not."""
+
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    return True if fuel_left * mpg >= distance_to_pump else False
