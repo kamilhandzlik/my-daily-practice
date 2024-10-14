d7971ba0bff100ae8a9cd9a864822a48993ebf03
Kamil Handzlik <kamol0209@gmail.com>
2024-10-14 20:04:33 +0200

    my daily practice

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -2213,7 +2213,7 @@ def position_polish(alphabet):
     alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z', 'ź', 'ż']
     return f"Position of alphabet {alphabet_list.index(alphabet) + 1}"
 
-
+'abcdefghijklmnopqrstuwxyz'
 # Solution 3
 def position(alphabet):
     return f"Position of alphabet:{ord(alphabet) - ord('a') + 1}"
@@ -2230,3 +2230,87 @@ Function should return true if it is possible and false if not."""
 
 def zero_fuel(distance_to_pump, mpg, fuel_left):
     return True if fuel_left * mpg >= distance_to_pump else False
+
+
+#########################################################################################################
+##################################            60            #############################################
+#########################################################################################################
+
+"""Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false."""
+
+def include(arr, item):
+    return item in arr
+
+
+
+#########################################################################################################
+##################################            61            #############################################
+#########################################################################################################
+
+"""Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0"""
+
+
+def get_char(c):
+    return chr(c)
+
+
+
+#########################################################################################################
+##################################            62            #############################################
+#########################################################################################################
+
+"""ntroduction
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+
+Task
+Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+The left side letters and their power:
+
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+The right side letters and their power:
+
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+The other letters don't have power and are only victims.
+
+Example
+AlphabetWar("z");        //=> Right side wins!
+AlphabetWar("zdqmwpbs"); //=> Let's fight again!
+AlphabetWar("zzzzs");    //=> Right side wins!
+AlphabetWar("wwwwwwz");  //=> Left side wins!"""
+
+
+def alphabet_war(fight):
+    left_side = {'w': 4, 'p': 3, 'b': 2, 's': 1}
+    right_side = {'m': 4, 'q': 3, 'd': 2, 'z': 1}
+
+    count_left = 0
+    count_right = 0
+
+    for letter in fight:
+        if letter in left_side:
+            count_left += left_side[letter]
+        elif letter in right_side:
+            count_right += right_side[letter]
+
+    if count_right > count_left:
+        return "Right side wins!"
+    elif count_left > count_right:
+        return "Left side wins!"
+    else:
+        return "Let's fight again!"
+
+
