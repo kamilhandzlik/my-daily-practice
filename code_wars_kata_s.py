e9cf0ad7e643faca4f45f19003b903607fd109dc
Kamil Handzlik <kamol0209@gmail.com>
2024-10-17 20:00:07 +0200

    my daily practice

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -2343,3 +2343,20 @@ def define_suit(card):
 # Solution 2
 def define_suit(card):
     return {'C': 'clubs', 'S': 'spades', 'D': 'diamonds', 'H': 'hearts'}[card[-1]]
+
+
+#########################################################################################################
+##################################            64            #############################################
+#########################################################################################################
+
+"""Combine strings function
+Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+With "James" as the first name and "Stevens" as the last name should return "James Stevens"""
+
+
+def combine_names(first_name, last_name):
+    return first_name + ' ' + last_name
+
