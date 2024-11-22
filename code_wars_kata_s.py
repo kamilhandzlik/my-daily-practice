1f6ad7c956b999edbca810e9753f5731974e8398
Kamil Handzlik <kamol0209@gmail.com>
2024-11-22 12:12:21 +0100

    my daily practice

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -3354,19 +3354,43 @@ def generate_random_test_85():
     return dad_years_old, son_years_old, expected
 
 
-class Test85(unittest.TestCase):
-    def test_85(self):
-        for (dad_years_old, son_years_old), expected in test_85.items():
-            with self.subTest(dad_years_old=dad_years_old, son_years_old=son_years_old, expected=expected):
-                self.assertEqual(twice_as_old(dad_years_old, son_years_old), expected)
+# class Test85(unittest.TestCase):
+#     def test_85(self):
+#         for (dad_years_old, son_years_old), expected in test_85.items():
+#             with self.subTest(dad_years_old=dad_years_old, son_years_old=son_years_old, expected=expected):
+#                 self.assertEqual(twice_as_old(dad_years_old, son_years_old), expected)
 
 
-    def random_test_85(self):
-        dad_years_old, son_years_old, expected = generate_random_test_85()
-        for _ in range(50):
-            with self.subTest(dad_years_old=dad_years_old, son_years_old=son_years_old, expected=expected):
-                self.assertEqual(twice_as_old(dad_years_old, son_years_old), expected)
+#     def random_test_85(self):
+#         dad_years_old, son_years_old, expected = generate_random_test_85()
+#         for _ in range(50):
+#             with self.subTest(dad_years_old=dad_years_old, son_years_old=son_years_old, expected=expected):
+#                 self.assertEqual(twice_as_old(dad_years_old, son_years_old), expected)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
+
+
+#########################################################################################################
+##################################            86            #############################################
+#########################################################################################################
+
+"""Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length."""
+
+
+def sort_by_length(arr):
+    return sorted(arr, key=len)
+
+
+
