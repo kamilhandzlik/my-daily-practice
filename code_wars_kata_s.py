552eaaf7593de48f5a2d280e9f4ab597a1a39b47
kamilhandzlik <kamol0209@gmail.com>
2024-12-11 18:01:10 +0100

    my daily practice

--- code_wars_kata_s.py
+++ code_wars_kata_s.py
@@ -3532,4 +3532,30 @@ def check_coupon(*, entered_code: str, correct_code: str, is_valid: bool) -> boo
     return entered_code is correct_code and is_valid
 
 
-#test
+#########################################################################################################
+##################################            89            #############################################
+#########################################################################################################
+
+"""
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+"""
+
+def max_sequence(arr):
+    sums = []
+    for j, e in enumerate(arr):
+        sums.append(e)
+        for i in range(j+1, len(arr)):
+            e += arr[i]
+            sums.append(e)
+    if all(i < 0 for i in arr) or not sums:
+        return 0
+    else:
+        return max(sums)
+
+
