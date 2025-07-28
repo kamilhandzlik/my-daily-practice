204184a11f5c538cc5a7f360c912c0fb5b07f303
kamol0209 <kamol0209@gmail.com>
2025-07-28 11:43:28 +0200

    daily practice

---
+++ code wars/7kyu/Coding_Meetup__1_- Higher-Ord_Functions_Series_-_Count_the_number_of_JavaScript_developers_coming_from_Europe.py
@@ -0,0 +1,50 @@
+"""
+You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+Your task is to return the number of JavaScript developers coming from Europe.
+
+For example, given the following list:
+
+lst1 = [
+  { 'firstName': 'Noah', 'lastName': 'M.', 'country': 'Switzerland', 'continent': 'Europe', 'age': 19, 'language': 'JavaScript' },
+  { 'firstName': 'Maia', 'lastName': 'S.', 'country': 'Tahiti', 'continent': 'Oceania', 'age': 28, 'language': 'JavaScript' },
+  { 'firstName': 'Shufen', 'lastName': 'L.', 'country': 'Taiwan', 'continent': 'Asia', 'age': 35, 'language': 'HTML' },
+  { 'firstName': 'Sumayah', 'lastName': 'M.', 'country': 'Tajikistan', 'continent': 'Asia', 'age': 30, 'language': 'CSS' }
+]
+your function should return number 1.
+
+If, there are no JavaScript developers from Europe then your function should return 0.
+
+Notes:
+
+The format of the strings will always be Europe and JavaScript.
+All data will always be valid and uniform as in the example above.
+
+
+
+
+This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the katas are of course possible.
+"""
+
+# Solution
+def count_developers(lst):
+    return sum(1 for dev in lst if dev['continent'] == 'Europe' and dev['language'] == 'JavaScript')
+
+
+# Solution on crack XD
+def count_developers(lst):
+    result = 0
+
+    for d in lst:
+        Europe = 0
+        Javascript = 0
+        for j in d.values():
+            if j == 'Europe':
+                Europe += 1
+            elif j == 'JavaScript':
+                Javascript += 1
+
+            if Europe == 1 and Javascript == 1:
+                result += 1
+
+    return result
