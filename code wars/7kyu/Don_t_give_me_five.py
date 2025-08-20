7f12808d9b07273402e86b8f613605eff8f922d1
kamol0209 <kamol0209@gmail.com>
2025-08-20 14:38:42 +0200

    daily practice

---
+++ code wars/7kyu/Don_t_give_me_five.py
@@ -0,0 +1,50 @@
+"""
+Don't give me five!
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+Examples:
+
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+The result may contain fives. ;-)
+The start number will always be smaller than the end number. Both numbers can be also negative!
+
+I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+
+
+"""
+
+
+# Solution 1
+def dont_give_me_five(start, end):
+    result = 0
+    for i in range(start, end + 1):
+        if '5' in str(i):
+            result += 0
+        else:
+            result += 1
+    return result
+
+
+# Solution 2
+def dont_give_me_five(start, end):
+    return len([i for i in range(start, end + 1) if '5' not in str(i)])
+
+
+# Solution 3
+def dont_give_me_five(start, end):
+    return sum('5' not in str(i) for i in range(start, end + 1))
+
+
+# Solution 4
+def dont_give_me_five(start, end):
+    result = 0
+    for i in range(start, end + 1):
+        if '5' not in str(i):
+            result += 1
+
+    return result
