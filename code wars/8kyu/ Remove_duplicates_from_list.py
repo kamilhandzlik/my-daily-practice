d30a1dbefc36852581cc6ee9224d8206ca97a2e0
kamol0209 <kamol0209@gmail.com>
2025-09-22 18:48:06 +0200

    my daily practice

---
+++ code wars/8kyu/ Remove_duplicates_from_list.py
@@ -0,0 +1,25 @@
+"""
+Remove duplicates from list
+
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+"""
+
+
+def distinct(seq):
+    seen = set()
+    result = []
+
+    for num in seq:
+        if num not in seen:
+            seen.add(num)
+            result.append(num)
+    return result
+
