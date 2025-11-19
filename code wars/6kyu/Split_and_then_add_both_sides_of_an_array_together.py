61ed78db194a46b1842f6b98affe7aed8ae38b74
kamol0209 <kamol0209@gmail.com>
2025-11-19 20:39:32 +0100

    daily practice

---
+++ code wars/6kyu/Split_and_then_add_both_sides_of_an_array_together.py
@@ -0,0 +1,64 @@
+"""
+Split and then add both sides of an array together.
+
+Task
+You will receive an array as parameter that contains 1 or more integers and a number n.
+
+Here is a little visualization of the process:
+
+Step 1: Split the array in two:
+
+[1, 2, 5, 7, 2, 3, 5, 7, 8]
+      /            \
+[1, 2, 5, 7]    [2, 3, 5, 7, 8]
+Step 2: Put the arrays on top of each other:
+
+   [1, 2, 5, 7]
+[2, 3, 5, 7, 8]
+Step 3: Add them together:
+
+[2, 4, 7, 12, 15]
+Repeat the above steps n times or until there is only one number left, and then return the array.
+
+Example
+Input: arr=[4, 2, 5, 3, 2, 5, 7], n=2
+
+
+Round 1
+-------
+step 1: [4, 2, 5]  [3, 2, 5, 7]
+
+step 2:    [4, 2, 5]
+        [3, 2, 5, 7]
+
+step 3: [3, 6, 7, 12]
+
+
+Round 2
+-------
+step 1: [3, 6]  [7, 12]
+
+step 2:  [3,  6]
+         [7, 12]
+
+step 3: [10, 18]
+
+
+Result: [10, 18]
+"""
+
+
+def split_and_add(numbers, n):
+    for  _ in range(n):
+        if len(numbers) == 1:
+            break
+        mid = len(numbers) // 2
+        left = numbers[:mid]
+        right = numbers[mid:]
+
+        if len(left) < len(right):
+            left = [0] * (len(right) - len(left)) + left
+        numbers = [l + r for l, r in zip(left, right)]
+    return numbers
+
+print(split_and_add([1, 2, 5, 7, 2, 3, 5, 7, 8], 2))
