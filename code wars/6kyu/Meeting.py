6cbda39daa3e896e1ada901cc4cd91cde0c1150d
kamol0209 <kamol0209@gmail.com>
2025-12-03 18:32:12 +0100

    daily practice

---
+++ code wars/6kyu/Meeting.py
@@ -0,0 +1,34 @@
+"""
+Meeting
+
+John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.
+
+Notes
+You can see another examples in the "Sample tests".
+
+"""
+
+# Solution 1
+def meeting(s):
+    people = s.split(";")
+    formatted = [(first.upper(), last.upper()) for first, last in (p.split(":") for p in people)]
+    formatted.sort(key=lambda x: (x[1], x[0]))
+
+    return "".join(f"({last}, {first})" for first, last in formatted)
+
+
+# Solution 2
+def meeting(s):
+    return ''.join(sorted('({1}, {0})'.format(*(x.split(':'))) for x in s.upper().split(';')))
