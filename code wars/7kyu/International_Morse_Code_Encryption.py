a49b70a10886164fa3808d59517123e0b8b9718a
kamol0209 <kamol0209@gmail.com>
2025-12-20 16:28:45 +0100

    daily practice

---
+++ code wars/7kyu/International_Morse_Code_Encryption.py
@@ -0,0 +1,24 @@
+"""
+International Morse Code Encryption
+
+Write a function that will encrypt a given sentence into International Morse Code, both the input and out puts will be strings.
+
+Characters should be separated by a single space. Words should be separated by a triple space.
+
+For example, "HELLO WORLD" should return -> ".... . .-.. .-.. --- .-- --- .-. .-.. -.."
+
+To find out more about Morse Code follow this link: https://en.wikipedia.org/wiki/Morse_code
+
+A preloaded object/dictionary/hash called CHAR_TO_MORSE will be provided to help convert characters to Morse Code.
+"""
+
+from preloaded import CHAR_TO_MORSE
+
+def encryption(string):
+    words = string.split(" ")
+    encoded_words = []
+
+    for word in words:
+        encoded_words.append(" ".join([CHAR_TO_MORSE[ch] for ch in word]))
+
+    return "   ".join(encoded_words)
