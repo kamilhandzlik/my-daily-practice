45a642eb54f475c793fed80f4b4d2846cea5b6de
kamol0209 <kamol0209@gmail.com>
2025-12-29 11:31:12 +0100

    my daily practice

---
+++ code wars/8kyu/Pillars.py
@@ -0,0 +1,17 @@
+"""
+Pillars
+
+There are pillars near the road. The distance between the pillars is the same and the width
+of the pillars is the same. Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters
+(without the width of the first and last pillar).
+"""
+
+# Solution 1
+def pillars(num_pill, dist, width):
+    return (dist * 100 + width) * (num_pill - 1) - width if num_pill > 1 else 0
+
