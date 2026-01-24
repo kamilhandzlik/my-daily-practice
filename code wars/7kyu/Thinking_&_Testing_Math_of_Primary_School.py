bd8cdb69902417f06f4ff790c8618d3da66b1a77
kamol0209 <kamol0209@gmail.com>
2026-01-24 16:50:33 +0100

    my daily practice

---
+++ code wars/7kyu/Thinking_&_Testing_Math_of_Primary_School.py
@@ -0,0 +1,64 @@
+"""
+Thinking & Testing : Math of Primary School
+
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at the results of the testcases, and guess the code!
+
+Series:
+A and B?
+Incomplete string
+True or False
+Something capitalized
+Uniq or not Uniq
+Spatiotemporal index
+Math of Primary School
+Math of Middle school
+From nothingness To nothingness
+Not perfect? Throw away!
+Welcome to take the bus
+A happy day will come
+Sum of 15(Hetu Luosliu)
+Nebula or Vortex
+Sport Star
+Falsetto Rap Concert
+Wind whispers
+Mobile phone simulator
+Join but not join
+I hate big and small
+I want to become diabetic ;-)
+How many blocks?
+Operator hidden in a string
+Substring Magic
+Report about something
+Retention and discard I
+Retention and discard II
+How many "word"?
+Hail and Waterfall
+Love Forever
+Digital swimming pool
+Archery contest
+The repair of parchment
+Who are you?
+Safe position
+Special recommendation
+Another series, innovative and interesting, medium difficulty. People who like challenges, can try these kata:
+
+Play Tetris : Shape anastomosis
+Play FlappyBird : Advance Bravely
+
+myjinxin2015
+https://www.codewars.com/kata/56d9b46113f38864b8000c5a/train/python
+"""
+
+
+def mystery(a):
+    return a[0] * a[3] + a[1] * a[2]
+
+
+print(mystery([1, 1, 0, 1]))
+
