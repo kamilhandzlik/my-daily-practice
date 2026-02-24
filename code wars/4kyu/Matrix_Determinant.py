80abc44396b67b72ce470ba23570c061a57f56f4
kamol0209 <kamol0209@gmail.com>
2026-02-24 20:19:52 +0100

    my daily practice

---
+++ code wars/4kyu/Matrix_Determinant.py
@@ -0,0 +1,61 @@
+"""
+Matrix Determinant
+
+Write a function that accepts a square matrix (N x N 2D array) and returns the determinant of the matrix.
+
+How to take the determinant of a matrix -- it is simplest to start with the smallest cases:
+
+A 1x1 matrix |a| has determinant a.
+
+A 2x2 matrix [ [a, b], [c, d] ] or
+
+|a  b|
+|c  d|
+has determinant: a*d - b*c.
+
+The determinant of an n x n sized matrix is calculated by reducing the problem to the calculation of the determinants of n matrices ofn-1 x n-1 size.
+
+For the 3x3 case, [ [a, b, c], [d, e, f], [g, h, i] ] or
+
+|a b c|
+|d e f|
+|g h i|
+the determinant is: a * det(a_minor) - b * det(b_minor) + c * det(c_minor) where det(a_minor) refers to taking the determinant of the 2x2 matrix created by crossing out the row and column in which the element a occurs:
+
+|- - -|
+|- e f|
+|- h i|
+Note the alternation of signs.
+
+The determinant of larger matrices are calculated analogously, e.g. if M is a 4x4 matrix with first row [a, b, c, d], then:
+
+det(M) = a * det(a_minor) - b * det(b_minor) + c * det(c_minor) - d * det(d_minor)
+
+
+pjfranzini
+https://www.codewars.com/kata/52a382ee44408cea2500074c/train/python
+"""
+
+
+def determinant(matrix):
+    n = len(matrix)
+
+    if n == 1:
+        return matrix[0][0]
+
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+
+    for col in range(n):
+        minor = [
+            row[:col] + row[col + 1:]
+            for row in matrix[1:]
+        ]
+
+        sign = (-1) ** col
+
+        det += sign * matrix[0][col] * determinant(minor)
+
+    return det
