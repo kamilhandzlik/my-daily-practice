6446ea9c1e5958876e43496e9d0f24f93ad7f17e
kamol0209 <kamol0209@gmail.com>
2026-04-16 19:09:02 +0200

    daily practice

---
+++ code wars/6kyu/Running_Average.py
@@ -0,0 +1,37 @@
+"""
+Running Average
+
+Python:
+Create a function running_average() that returns a callable function object. Update the series with each given value and calculate the current average.
+
+r_avg = running_average()
+r_avg(10) = 10.0
+r_avg(11) = 10.5
+r_avg(12) = 11
+All input values are valid. Round to two decimal places.
+
+This Kata is based on a example from Fluent Python book.
+
+Javascript // Lua // C++:
+Create a function runningAverage() that returns a callable function object. Update the series with each given value and calculate the current average.
+
+rAvg = runningAverage();
+rAvg(10) = 10.0;
+rAvg(11) = 10.5;
+rAvg(12) = 11;
+
+user7657844
+https://www.codewars.com/kata/589e4d646642d144a90000d8/train/python
+"""
+
+def running_average():
+    total = 0
+    count = 0
+
+    def inner(value):
+        nonlocal total, count
+        total += value
+        count += 1
+        return round(total / count, 2)
+
+    return inner
